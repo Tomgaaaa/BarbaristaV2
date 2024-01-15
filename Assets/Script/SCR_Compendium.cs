bae4571d11b730a08f9710de6115cbfec4de59ae
Feyndrir <simon.brillet@outlook.fr>
2024-01-15 16:04:31 +0100

    UI Compendium Base Sommaire

---
+++ Assets/Script/SCR_Compendium.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Compendium : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> Page;
+    [SerializeField]private int ActualPage;
+
+    public void NextPage()
+    {
+        
+    }
+
+    public void PreviewPage()
+    {
+
+    }
+
+    public void GoToPage(int page)
+    {
+
+    }
+}
