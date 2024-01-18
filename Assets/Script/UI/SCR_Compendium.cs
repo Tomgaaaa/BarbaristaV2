3903896c97250d2e9b38380be0a583adcf0a3030
Feyndrir <simon.brillet@outlook.fr>
2024-01-18 15:50:33 +0100

    Sous biome prfab compendium

--- Assets/Script/UI/SCR_Compendium.cs
+++ Assets/Script/UI/SCR_Compendium.cs
@@ -6,10 +6,13 @@ public class SCR_Compendium : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _page;
     [SerializeField]private int _currentPage;
-    
+    [SerializeField] private AudioSource _audio;
+    [SerializeField] private AudioClip _simpleFlipPage;
+
     public virtual void NextPage()
     {
-        
+        _audio.PlayOneShot(_simpleFlipPage);
+
         _page[_currentPage].SetActive(false);
         _page[_currentPage + 1].SetActive(true);
         _currentPage++;
@@ -18,6 +21,7 @@ public class SCR_Compendium : MonoBehaviour
 
     public virtual void PreviewPage()
     {
+        _audio.PlayOneShot(_simpleFlipPage);
         _page[_currentPage].SetActive(false);
         _page[_currentPage - 1].SetActive(true);
         _currentPage--;
@@ -25,6 +29,7 @@ public class SCR_Compendium : MonoBehaviour
 
     public virtual void GoToPage(int page)
     {
+        _audio.PlayOneShot(_simpleFlipPage);
         _page[_currentPage].SetActive(false);
         _page[page].SetActive(true);
         _currentPage = page;
