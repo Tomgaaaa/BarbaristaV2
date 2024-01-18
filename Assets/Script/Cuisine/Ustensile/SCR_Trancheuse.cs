b81fe49050280f39dff8d61ae997a5a7aa3b0d3f
TomGaaa <gaetanguyon974@gmail.com>
2024-01-18 17:29:11 +0100

    fin bouilloire debut trancheuse

--- Assets/Script/Cuisine/Ustensile/SCR_Trancheuse.cs
+++ Assets/Script/Cuisine/Ustensile/SCR_Trancheuse.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +11,14 @@ public class SCR_Trancheuse : SCR_Ustensile
 
     private Vector3 lastMousePos;
     private float RotZ;
+    private Tween tweenRotationDrag;
 
-   
+    private void OnMouseDown()
+    {
+        lastMousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
 
     public override void OnMouseDrag()
     {
@@ -24,14 +31,33 @@ public class SCR_Trancheuse : SCR_Ustensile
 
             Vector3 diffMousePos = mainCam.ScreenToWorldPoint(Input.mousePosition) - lastMousePos;
             RotZ += diffMousePos.x + diffMousePos.y;
-            float rotzClamp = Mathf.Clamp(RotZ, 0, 1);
+            float rotzClamp = Mathf.Clamp(RotZ, -1, 0);
             RotZ = rotzClamp;
-            Debug.Log(RotZ);
-            //float rotationXRemap = Remap(rotzClamp, 0, 1, 5, -25);
+            float rotationXRemap = Remap(rotzClamp, 0, -1, -51, -5);
+
+            //baseTrancheuse.rotation = Quaternion.Euler(0, 0, rotationXRemap);
+            //Mathf.Lerp(baseTrancheuse.rotation.eulerAngles.z, Quaternion.Euler(0, 0, rotationXRemap), baseTrancheuse.rotation.eulerAngles.z);
+            tweenRotationDrag = baseTrancheuse.DORotate(new Vector3(0,0,rotationXRemap), 1f);
 
             lastMousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
         }
+    }
+
+
+    private void OnMouseUp()
+    {
+        tweenRotationDrag.Kill();
+        //baseTrancheuse.rotation = Quaternion.Euler(0, 0, -51f);
+
+        tweenRotationDrag = baseTrancheuse.DORotate(new Vector3(0, 0, -51), 1.2f) ;
+
+    }
+
+    public override void FinishManipulation()
+    {
+        base.FinishManipulation();
+
     }
 
 }
