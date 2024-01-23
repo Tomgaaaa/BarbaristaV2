641066e04017348068bcf41cb821228f176c36e7
Feyndrir <simon.brillet@outlook.fr>
2024-01-23 16:17:02 +0100

    Dico page + prefab variant pour page

--- Assets/Script/UI/SCR_Compendium.cs
+++ Assets/Script/UI/SCR_Compendium.cs
@@ -2,13 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SCR_Compendium : MonoBehaviour
+public enum enumPage { 
+
+    Sommaire,
+
+    SommaireBiome,
+    BiomeGlace, BiomeFeu, BiomeElec, BiomeForet,
+    SousBMontagne, SousBGrotte, SousBTaiga, SousBVolcan, SousBDune, SousBRocheuse,
+    SousBFoudre, SousBHelium, SousBSoufre,SousBJungle, SousBMarecage, SousBBiolumi,
+
+    SommaireBestiaire,
+    FrengMontagne, FrengGrotte, MagastalMontagne, MagastalTaiga, SregolGrotte, ChokaroTaiga, SregolBasalte, DeshrogBasalte, CarabosDune,
+    EvisparDune, CarabosRocheux, KerusculaFoudre, CresholdSoufre, CresholdHelium, ChokaroHelium, KerusculaJungle, PhacoliereJungle, PhacoliereMarais,
+    EvisparMarais, EvisparBiolumi,
+
+    SommaireIngredient,
+    Frejal, Kleck, Phylliul, Scolk, Shembo, Siarym }
+
+public class SCR_Compendium : MonoBehaviour, ISerializationCallbackReceiver
 {
     [SerializeField] private List<GameObject> _page;
     [SerializeField]private int _currentPage;
     [SerializeField] private AudioSource _audio;
     [SerializeField] private AudioClip _simpleFlipPage;
 
+    [System.Serializable] public class dicoPageClass : TemplateDico<enumPage, GameObject> { };
+    [SerializeField] List<dicoPageClass> dicoPageList;
+
+    private Dictionary<enumPage, GameObject> dicoPage;
     public virtual void NextPage()
     {
         _audio.PlayOneShot(_simpleFlipPage);
@@ -33,5 +54,23 @@ public class SCR_Compendium : MonoBehaviour
         _page[_currentPage].SetActive(false);
         _page[page].SetActive(true);
         _currentPage = page;
+    }
+
+    public void OnBeforeSerialize()
+    {
+       
+    }
+
+    public void OnAfterDeserialize()
+    {
+        dicoPage = new Dictionary<enumPage, GameObject>();
+        foreach(dicoPageClass item in dicoPageList)
+        {
+            if(!dicoPage.ContainsKey(item.key))
+            {
+                dicoPage.Add(item.key, item.value);
+            }
+        }
+        
     }
 }
