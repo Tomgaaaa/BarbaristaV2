d491360825ce534cdd87db9a06f13590e03abc8f
Feyndrir <simon.brillet@outlook.fr>
2024-01-30 14:42:12 +0100

    Reward quete fonctionnel

---
+++ Assets/Script/UI/Quete/SCR_MasterQuete.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCR_MasterQuete : MonoBehaviour
+{
+    [SerializeField] SO_Quete queteInfo;
+    [SerializeField] Text titre;
+    [SerializeField] Text description;
+    [SerializeField] Image illu;
+    [SerializeField] Transform reward;
+    [SerializeField] Transform diff;
+
+
+
+    private void Awake()
+    {
+        
+        titre.text = queteInfo.titre;
+        description.text = queteInfo.description;
+        illu.sprite = queteInfo.illustration;
+
+        foreach(Image etoile in queteInfo.difficulty)
+        {
+            Instantiate<Image>(queteInfo.difficulty[0], diff);
+        }
+
+        for (int i = 0; i < queteInfo.reward.Count;i++)
+        {
+            
+            Instantiate<Image>(queteInfo.reward[i], reward);
+            
+        }
+    }
+}
---
+++ Assets/Script/UI/Quete/SO_Quete.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "Quete", menuName = "ScriptableObjects/QueteInfo")]
+public class SO_Quete : ScriptableObject, ISerializationCallbackReceiver
+{
+    [System.Serializable] public class dicoResistanceClass : TemplateDico<enumResistance, float> { };
+    public Dictionary<enumResistance, float> dicoResistance; // dictionnaire des resistances, on associe une resistance à un float, float psk pour bouger dans l'hexagone il faut un float
+    [SerializeField] private List<dicoResistanceClass> listDicoResistance; // permet de visualiser le dico des résistances, en private psk on a pas besoin d'y toucher, c'est juste pour visualiser en éditor
+
+    public string titre;
+    public List<Image> difficulty;
+    public Sprite illustration; 
+    public string description;
+    public List<Image> reward;
+
+    public void OnAfterDeserialize() // fonction qui permet d'associer les listes (qu'on voit) aux dictionaires (qu'on ne voit pas)
+    {
+
+        dicoResistance = new Dictionary<enumResistance, float>();
+        foreach (dicoResistanceClass item in listDicoResistance)
+        {
+            if (!dicoResistance.ContainsKey(item.key))
+            {
+                dicoResistance.Add(item.key, item.value);
+            }
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
+    }
+}
