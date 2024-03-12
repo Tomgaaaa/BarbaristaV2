eafe5502dfa6a1ab0e71832fb7ee4b1f76ef6fb5
TomGaaa <gaetanguyon974@gmail.com>
2024-03-12 13:24:31 +0100

    debut cageot, gain ingredient

---
+++ Assets/Script/Gain/SCR_GainIngredient.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static SCR_QueteManager;
+
+public class SCR_GainIngredient : MonoBehaviour, ISerializationCallbackReceiver
+{
+    #region dico
+    [System.Serializable] public class dicoIngredientSpriteClass : TemplateDico<enumAllIgredient, Sprite> { };
+    private Dictionary<enumAllIgredient, Sprite> dicoIngredientSprite; 
+    [SerializeField] private List<dicoIngredientSpriteClass> listDicoIngredientSprite;
+    public void OnBeforeSerialize()
+    {
+    }
+
+    public void OnAfterDeserialize()
+    {
+        dicoIngredientSprite = new Dictionary<enumAllIgredient, Sprite>();
+
+        foreach (dicoIngredientSpriteClass item in listDicoIngredientSprite)
+        {
+            if (!dicoIngredientSprite.ContainsKey(item.key))
+            {
+                dicoIngredientSprite.Add(item.key, item.value);
+            }
+        }
+    }
+
+    #endregion
+
+    [SerializeField] private List<enumAllIgredient> listDailyIngredient;
+    [SerializeField] private List<enumAllIgredient> listDataIngredient;
+    [SerializeField] Transform transforCadrillage;
+    [SerializeField] float delaySpawnIngredient;
+    [SerializeField] float spacingXIngredient;
+    [SerializeField] float spacingYIngredient;
+    [SerializeField] int colonne;
+    [SerializeField] SpriteRenderer prefabIngredient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    [ContextMenu("Spawn")]
+    private void DebugSpawn()
+    {
+        StartCoroutine(SpawnDailyIngredient());
+    }
+
+
+    private IEnumerator SpawnDailyIngredient()
+    {
+        float spacingY = 0f;
+        float offSetX = 0f;
+      
+
+        for (int i = 0 ; i< listDailyIngredient.Count;i++)
+        {
+            SpriteRenderer ingredientSpawn = Instantiate(prefabIngredient,transforCadrillage);
+            ingredientSpawn.sprite = dicoIngredientSprite[listDailyIngredient[i]];
+            ingredientSpawn.transform.localPosition = new Vector3 ((i- offSetX) * spacingXIngredient ,-spacingY,0);
+
+            if(i == colonne -1 )
+            {
+                spacingY += spacingYIngredient;
+                offSetX = i +1;
+            }
+
+            yield return new WaitForSeconds(delaySpawnIngredient);
+        }
+
+        yield return null;
+    }
+
+
+    private IEnumerator SpawnDaTaIngredient()
+    {
+        float spacingY = 0f;
+        float offSetX = 0f;
+
+
+        for (int i = 0; i < listDataIngredient.Count; i++)
+        {
+            SpriteRenderer ingredientSpawn = Instantiate(prefabIngredient, transforCadrillage);
+            ingredientSpawn.sprite = dicoIngredientSprite[listDailyIngredient[i]];
+            ingredientSpawn.transform.localPosition = new Vector3((i - offSetX) * spacingXIngredient, -spacingY, 0);
+
+            if (i == colonne - 1)
+            {
+                spacingY += spacingYIngredient;
+                offSetX = i + 1;
+            }
+
+            yield return new WaitForSeconds(delaySpawnIngredient);
+        }
+
+        yield return null;
+    }
+
+
+}
